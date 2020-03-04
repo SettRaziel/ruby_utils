6187af3bc734b95c7fb1f786a63d3ef39c8c186e
Benjamin Held <me@bheld.eu>
2020-03-04 16:32:03 +0100

    added methods for extreme values and added tests

--- lib/ruby_utils/statistic.rb
+++ lib/ruby_utils/statistic.rb
@@ -1,11 +1,11 @@
 # @Author: Benjamin Held
 # @Date:   2016-04-24 18:33:55
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-02-25 19:33:40
+# @Last Modified time: 2020-03-04 16:24:39
 
 module RubyUtils
 
-  # This module holds methods to apply statistical functions of the provided data
+  # This class holds methods to apply statistical functions of the provided data
   class Statistic
 
     # method to calculate the arithmetic mean values of the given data
@@ -21,13 +21,59 @@ module RubyUtils
       mean / data.length
     end
 
+    # method to determine the maximum value of the given data
+    # @param [Array] data a list of values
+    # @return [Float] the maximum value of the input data
     def self.max_value(data)
       max_value = data[0]
-      data.each { | value|
+      data.each { |value|
         max_value = value if (value > max_value)
       }
 
       return max_value
+    end
+
+    # method to determine the minimum value of the given data
+    # @param [Array] data a list of values
+    # @return [Float] the minimum value of the input data
+    def self.min_value(data)
+      min_value = data[0]
+      data.each { |value|
+        min_value = value if (value < min_value)
+      }
+
+      return min_value
+    end
+
+    # method to determine the extreme value min and max of the given data
+    # @param [Array] data a list of values
+    # @return [Array] the minimum value of the input data
+    def self.extreme_values(data)
+      max_value = data[0]
+      min_value = data[0]
+      data.each { |value|
+        max_value = value if (value > max_value)
+        min_value = value if (value < min_value)
+      }      
+      return ExtremeValues.new(min_value, max_value)
+    end
+
+  end
+
+  # simple helper class to store the minimal and maximal value for a given data set
+  class ExtremeValues
+
+    # @return [Numeric] the determined maximum value
+    attr_reader :maximum
+    # @return [Numeric] the determined minimum value
+    attr_reader :minimum
+
+    # initialization
+    # @param [Numeric] min_value the minimum value
+    # @param [Numeric] max_value the maximum value    
+    def initialize(min_value, max_value)
+      @minimum = min_value
+      @maximum = max_value
     end
 
   end
---
+++ spec/math/statistic_spec.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby
+# @Author: Benjamin Held
+# @Date:   2020-03-03 21:47:25
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2020-03-04 16:27:15
+
+require 'spec_helper'
+require 'ruby_utils/statistic'
+
+describe RubyUtils::Statistic do
+
+  describe "#mean_value" do
+    context "given an array of values" do
+      it "calculate the mean value" do
+        data = [1.0, 2.0, 3.0, 3.0, 4.0, 5.0]
+        expect(RubyUtils::Statistic.mean_value(data)).to match(3.0)
+      end
+    end
+  end
+
+  describe "#max_value" do
+    context "given an array of values" do
+      it "determine the maximum value" do
+        data = [1.0, 2.0, 5.0, 3.0, 4.0, 5.0]
+        expect(RubyUtils::Statistic.max_value(data)).to match(5.0)
+      end
+    end
+  end
+
+  describe "#min_value" do
+    context "given an array of values" do
+      it "determine the minimum value" do
+        data = [1.0, 2.0, 3.0, 1.0, 4.0, 5.0]
+        expect(RubyUtils::Statistic.min_value(data)).to match(1.0)
+      end
+    end
+  end
+
+  describe "#extreme_values" do
+    context "given an array of values" do
+      it "determine the extreme value" do
+        data = [1.0, 2.0, 3.0, 1.0, 4.0, 5.0, 3.0, 5.0, 3.5, 6.3]
+        extreme_values = RubyUtils::Statistic.extreme_values(data)
+        expect(extreme_values.minimum).to match(1.0)
+        expect(extreme_values.maximum).to match(6.3)
+      end
+    end
+  end
+
+end
