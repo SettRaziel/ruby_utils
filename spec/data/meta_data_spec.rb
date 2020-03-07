22954509d2ca0cf6d204dd4f76426f5a11207476
Benjamin Held <me@bheld.eu>
2020-03-07 21:50:55 +0100

    added test for meta data

---
+++ spec/data/meta_data_spec.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+# @Author: Benjamin Held
+# @Date:   2020-03-07 20:19:02
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2020-03-07 20:21:43
+
+require 'spec_helper'
+
+describe RubyUtils::MetaData do
+
+  describe ".new" do
+    context "given an array with meta data" do
+      it "raise the not implemented error from the abstract method" do
+        arguments = ['Sample', 'Meta', 'Data']
+        expect { 
+          RubyUtils::MetaData.new(arguments)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+end
