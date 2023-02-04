76e3fecdd10c6f25c7c7edfffc84c05297f195d3
Benjamin Held <me@bheld.eu>
2023-02-04 20:47:12 +0100

    remove unused file headers

--- create_yard.sh
+++ create_yard.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# @Author: Benjamin Held
-# @Date:   2015-08-30 09:48:37
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-03-26 17:32:15
 
 yardoc -q './lib/**/*.rb' --private --readme README.md
 printf "Looking for undocumented passages: \n"
