7393397c8731000d3dedc936443075bb6281bfe1
Benjamin Held <me@bheld.eu>
2024-02-25 13:13:41 +0100

    add error tests for unimplemented help_output methods

--- spec/help_output/base_help_output_spec.rb
+++ spec/help_output/base_help_output_spec.rb
@@ -38,4 +38,74 @@ describe RubyUtils::BaseHelpOutput do
     end
   end
 
+  describe "#add_single_help_entries" do
+    context "given the private method add_single_help_entries" do
+      it "try to call the method and throw an exception" do
+        expect {
+          RubyUtils::BaseHelpOutput.send(:add_single_help_entries)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+  describe "#add_one_argument_help_entries" do
+    context "given the private method add_one_argument_help_entries" do
+      it "try to call the method and throw an exception" do
+        expect {
+          RubyUtils::BaseHelpOutput.send(:add_one_argument_help_entries)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+  describe "#add_two_argument_help_entries" do
+    context "given the private method add_two_argument_help_entries" do
+      it "try to call the method and throw an exception" do
+        expect {
+          RubyUtils::BaseHelpOutput.send(:add_two_argument_help_entries)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+  describe "#print_help_head" do
+    context "given the private method print_help_head" do
+      it "try to call the method and throw an exception" do
+        expect {
+          RubyUtils::BaseHelpOutput.send(:print_help_head)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+  describe "#print_invalid_combinations" do
+    context "given the private method print_invalid_combinations" do
+      it "try to call the method and throw an exception" do
+        expect {
+          RubyUtils::BaseHelpOutput.send(:print_invalid_combinations)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+  describe "#print_configuration_parameter" do
+    context "given the private method print_configuration_parameter" do
+      it "try to call the method and throw an exception" do
+        expect {
+          RubyUtils::BaseHelpOutput.send(:print_configuration_parameter)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+  describe "#get_script_name" do
+    context "given the private method get_script_name" do
+      it "try to call the method and throw an exception" do
+        expect {
+          RubyUtils::BaseHelpOutput.send(:get_script_name)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
 end
